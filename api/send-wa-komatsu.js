7977b6180ba0d7faffe661194d09692db4b6f0c2
rrr-web <117338184+rrr-web@users.noreply.github.com>
2025-04-26 19:36:43 +0700

    Update send-wa-komatsu.js

--- api/send-wa-komatsu.js
+++ api/send-wa-komatsu.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     }
   
     const data = req.body;
-    const groupids = ['6285222707916-1558930545','6281273133989-1556927028'];
+    const groupIds = ['6285222707916-1558930545','6281273133989-1556927028'];
     // const groupIds = ['6282236924872'];
 
     const message = formatMessage(data);
