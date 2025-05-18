cfbb24473152737fb635142c3d2616397a0a0af0
rrr-web <117338184+rrr-web@users.noreply.github.com>
2025-05-18 09:59:46 +0700

    Update send-wa-ast.js

--- api/send-wa-ast.js
+++ api/send-wa-ast.js
@@ -8,8 +8,8 @@ export default async function handler(req, res) {
     }
 
   const data = req.body;
-  const groupIds = ['6281273133989-1502490848'];
-  // const groupIds = ['6282236924872'];
+  // const groupIds = ['6281273133989-1502490848'];
+  const groupIds = ['6282236924872'];
   
   const message = formatMessage(data);
 
